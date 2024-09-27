60830fecd0024213be7982b4ad4fdcc11b04acc9
laurenyip <laurenyip10@gmail.com>
2024-09-26 17:01:34 -0700

    eod

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,13 @@
 from flask import Flask, jsonify
-from flask_cors import CORS  # Import CORS
-from scraping import scrape_articles
-
+from scraper import scrape_articles
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
-@app.route('/api/scrape', methods=['GET'])
+@app.route('/scrape', methods=['GET'])
 def scrape():
-    url = 'https://laurenyip.com'  # Replace with the actual URL
-    titles = scrape_articles(url)
-    return jsonify(titles)
+    url = 'https://laurenyip.com'  # Replace with the actual URL you want to scrape
+    articles = scrape_articles(url)
+    return jsonify(articles)  # Return the scraped articles as JSON
 
 if __name__ == "__main__":
     app.run(debug=True)
